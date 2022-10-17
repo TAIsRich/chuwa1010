b64a40bac13ff9c4e304291f5d16c41b1b699cef
Ke Ma <ke.ma@uconn.edu>
2022-10-17 15:40:03 -0400

    Add files via upload

---
+++ Coding/HW3-Design/WhatsappNotification.java
@@ -0,0 +1,8 @@
+public class WhatsappNotification implements Notification{
+    @Override
+    public void notifyUser() {
+        System.out.println("Sending a Whatsapp Notification:");
+        System.out.println("Hey firstName, you have successfully registered to add and here is your , please use this for future references.");
+
+    }
+}
