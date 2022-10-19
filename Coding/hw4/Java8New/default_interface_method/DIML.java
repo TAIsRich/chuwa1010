6cd0784a056f7ac2f51ea3302bd1a33fd50e4327
LisaRiza <49122256+LisaRiza@users.noreply.github.com>
2022-10-18 21:48:59 -0400

    Create DIML.java

---
+++ Coding/hw4/Java8New/default_interface_method/DIML.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public interface DIML {
+    static final String BLOG = "is chuwa a";
+
+    int add(int a, int b);
+
+    default int substract(int a, int b){
+        return a - b;
+    }
+
+    static String blogName(){
+        return BLOG;
+    }
+}
