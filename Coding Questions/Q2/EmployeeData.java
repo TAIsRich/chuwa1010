a8ef2b7c61a9f91455aea308750ae64f78b89c35
0000llliii <109409370+0000llliii@users.noreply.github.com>
2022-10-19 11:48:54 -0700

    Q2

---
+++ Coding Questions/Q2/EmployeeData.java
@@ -0,0 +1,21 @@
+package hw4.Q2.stream_api;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class EmployeeData {
+
+    public static List<Employee> getEmployees() {
+        List<Employee> list = new ArrayList<>();
+
+        list.add(new Employee(1001, "Yun Ma", 50, 30000.00));
+        list.add(new Employee(1002, "Huateng Ma", 49, 22222.22));
+        list.add(new Employee(1004, "Jun Lei", 43, 12234.12));
+        list.add(new Employee(1005, "Bill Gates", 65, 999999));
+        list.add(new Employee(1003, "Yanhong Li", 30, 123123));
+        list.add(new Employee(1007, "Zhengfei Ren", 78, 66666));
+        list.add(new Employee(1006, "Mark Elliot Zuckerberg", 29, 88888));
+
+        return list;
+    }
+}
