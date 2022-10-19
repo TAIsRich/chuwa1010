cb420431b23f28d81282cd0a098cd4f6f6e31c84
LisaRiza <49122256+LisaRiza@users.noreply.github.com>
2022-10-18 22:59:51 -0400

    Add files via upload

---
+++ Coding/hw4/Java8New/method_reference/MethodReference.java
@@ -0,0 +1,9 @@
+package org.example.MReference;
+
+public class MethodReference {
+    //method reference to static method
+
+    public static void printHi(){
+        System.out.println("Hi.");
+    }
+}
