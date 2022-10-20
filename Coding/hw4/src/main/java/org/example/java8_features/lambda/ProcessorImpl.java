ffe51ea99f1f55494c27a37196d28030ce0a00d2
JXia7 <jclnbill@gmail.com>
2022-10-19 22:45:48 -0700

    update_hw4_generic_and_java8

---
+++ Coding/hw4/src/main/java/org/example/java8_features/lambda/ProcessorImpl.java
@@ -0,0 +1,16 @@
+package org.example.java8_features.lambda;
+
+import java.util.concurrent.Callable;
+import java.util.function.Supplier;
+
+public class ProcessorImpl implements Processor {
+    @Override
+    public String process(Callable<String> c) throws Exception {
+        return "Callable";
+    }
+
+    @Override
+    public String process(Supplier<String> s) {
+        return "Supplier";
+    }
+}
---
+++ Coding/hw4/src/main/java/org/example/java8_features/optional/Client.java
@@ -0,0 +1,28 @@
+package org.example.java8_features.optional;
+
+import java.util.Optional;
+
+public class Client {
+
+    public int getMobileScreenWidth(Mobile7 mobile7) {
+        if (mobile7 != null) {
+            DisplayFeatures7 df = mobile7.getDisplayFeatures();
+            if (df != null) {
+                ScreenResolution sr = df.getResolution();
+                if (sr != null) {
+                    return sr.getWidth();
+                }
+            }
+        }
+
+        return 0;
+    }
+
+    public int getMobileScreenWidthOptional(Mobile mobile) {
+        return Optional.ofNullable(mobile).flatMap(Mobile::getDisplayFeaturesOptional)
+                .flatMap(DisplayFeatures::getResolutionOptional)
+                .map(ScreenResolution::getWidth)
+                .orElse(0);
+    }
+}
+
