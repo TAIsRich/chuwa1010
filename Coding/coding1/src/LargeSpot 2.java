80e11d34e227e8b6ee1125d7d9b80cb175600696
xiyiji <btzdnsn@gmail.com>
2022-10-24 18:46:58 -0700

    fix

--- Coding/coding1/src/LargeSpot 2.java
+++
@@ -1,5 +0,0 @@
-public class LargeSpot extends ParkingSpot {
-    public LargeSpot() {
-        super(ParkingSpotType.LARGE);
-    }
-}
--- MavenProject/DesignPattern/src/main/java/FactoryPattern/factory_method/LatteeCoffeeFactory 2.java
+++
@@ -1,8 +0,0 @@
-package FactoryPattern.factory_method;
-
-public class LatteeCoffeeFactory implements CoffeeFactory{
-    @Override
-    public Coffee createCoffee() {
-        return new LatteeCoffee();
-    }
-}
