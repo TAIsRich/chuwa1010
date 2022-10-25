6b1c07ca3473c7a13bf9159c05e7b090e5625bae
xiyiji <btzdnsn@gmail.com>
2022-10-24 18:38:48 -0700

    fix conflict

---
+++ MavenProject/DesignPattern/src/main/java/SingletonPattern/EagerLoad/Singleton 2.java
@@ -0,0 +1,12 @@
+package SingletonPattern.EagerLoad;
+
+public class Singleton {
+    private Singleton() {}
+    private static Singleton instance;
+    static {
+        instance = new Singleton();
+    }
+    public static Singleton getInstance() {
+        return instance;
+    }
+}
