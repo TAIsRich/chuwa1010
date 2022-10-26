7ce8060515099a5e069ab5d71b1b12be04ce6668
Lin27604 <70904267+Lin27604@users.noreply.github.com>
2022-10-26 17:41:39 -0500

    Add files via upload

---
+++ mySqlquery.sql
@@ -0,0 +1,4 @@
+SELECT * FROM practicedb.oms_company_address;
+SELECT * FROM practicedb.oms_company_address LIMIT 3;
+UPDATE practicedb.oms_company_address SET phone =666-6666-8888;
+DELETE FROM practicedb.oms_company_address WHERE id=1; 
