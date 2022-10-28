613901d7c75b8039e0da153e0a9254ce63bd0ac5
xiyiji <btzdnsn@gmail.com>
2022-10-28 15:28:03 -0700

    howework40

---
+++ Coding/redbook/src/main/java/com/example/redbook/entity/Post.java
@@ -0,0 +1,77 @@
+package com.example.redbook.entity;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "posts",
+        uniqueConstraints = {@UniqueConstraint(columnNames = {"title"})}
+)
+
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private Long id;
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "description", nullable = false)
+    private String description;
+
+    @Column(name = "content", nullable = false)
+    private String content;
+
+    @CreationTimestamp
+    private LocalDateTime createDateTime;
+
+    public Post() {}
+
+    public Post(Long id, String description, String content) {
+        this.id = id;
+        this.description = description;
+        this.content = content;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public LocalDateTime getCreateDateTime() {
+        return createDateTime;
+    }
+
+    public void setCreateDateTime(LocalDateTime createDateTime) {
+        this.createDateTime = createDateTime;
+    }
+}
