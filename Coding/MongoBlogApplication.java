5bde424959df30ddef489bbc40c446e029f80957
0000llliii <109409370+0000llliii@users.noreply.github.com>
2022-10-28 12:35:47 -0700

    Add files via upload

---
+++ Coding/MongoBlogApplication.java
@@ -0,0 +1,15 @@
+package com.chuwa.mongoblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+@SpringBootApplication
+
+public class MongoBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongoBlogApplication.class, args);
+	}
+
+}
