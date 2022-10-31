7068597db6de351513eb2a99259aed57fd31df97
Jiayi Liu <amelia@dhcp-vl2043-1000.cornell-visitor.cit.cornell.edu>
2022-10-31 12:50:52 -0400

    hw7 finished

---
+++ Coding/MavenProject/redbook/src/main/java/com/chuwa/redbook/service/impl/PostServiceImpl.java
@@ -0,0 +1,118 @@
+package com.chuwa.redbook.service.impl;
+
+import com.chuwa.redbook.dao.PostRepository;
+import com.chuwa.redbook.entity.Post;
+import com.chuwa.redbook.exception.ResourceNotFoundException;
+import com.chuwa.redbook.payload.PostDto;
+import com.chuwa.redbook.service.PostService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class PostServiceImpl implements PostService {
+
+    private PostRepository postRepository;
+
+    public PostServiceImpl(PostRepository postRepository) {
+        this.postRepository = postRepository;
+    }
+
+    @Override
+    public PostDto createPost(PostDto postDto) {
+
+        //把payload转换成entity，这样才能dao去把数据存到数据库中
+//        Post post = new Post();
+//        post.setTitle(postDto.getTitle());
+//        post.setDescription(postDto.getDescription());
+//        post.setContent(postDto.getContent());
+        //此时已经成功把request body的信息传递到entity中
+
+        // convert DTO to entity
+        Post post = mapToEntity(postDto);
+
+        //调用dao的save方法，将entity的数据存到数据库MYSQL
+        //save()会返回存在数据库中的数据
+        Post savedPost = postRepository.save(post);
+
+        // convert entity to DTO
+        //将save()方法返回的数据转换成controller/前端需要的数据，然后return给controller
+//        PostDto postResponse = new PostDto();
+//        postResponse.setId(savedPost.getId());
+//        postResponse.setTitle(savedPost.getTitle());
+//        postResponse.setDescription(savedPost.getDescription());
+//        postResponse.setContent(savedPost.getContent());
+
+        PostDto postResponse = mapToDTO(savedPost);
+
+        return postResponse;
+    }
+
+    /**
+     * 此处练习了lambda， stream API
+     * @return
+     */
+    @Override
+    public List<PostDto> getAllPost() {
+        List<Post> posts = postRepository.findAll();
+        List<PostDto> postDtos = posts.stream().map(post -> mapToDTO(post)).collect(Collectors.toList());
+        return postDtos;
+    }
+
+    /**
+     * 此处顺便练习Optional
+     * @param id
+     * @return
+     */
+    @Override
+    public PostDto getPostById(long id) {
+//        Optional<Post> post = postRepository.findById(id);
+//        post.orElseThrow(() -> new ResourceNotFoundException("Post", "id", id));
+
+//        Post post = postRepository.findById(id).get();
+
+        //postRepository.findById(id)返回的是optional,因为可能数据库中没有，
+        Post post = postRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Post", "id", id));
+
+        return mapToDTO(post);
+    }
+
+    @Override
+    public PostDto updatePost(PostDto postDto, long id) {
+        //  Question, why do we need to find it out firstly?
+        Post post = postRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Post", "id", id));
+        post.setTitle(postDto.getTitle());
+        post.setDescription(postDto.getDescription());
+        post.setContent(postDto.getContent());
+
+        Post updatePost = postRepository.save(post);
+        return mapToDTO(updatePost);
+    }
+
+    @Override
+    public void deletePostById(long id) {
+        //  Question, why do we need to find it out firstly?
+        Post post = postRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Post", "id", id));
+        postRepository.delete(post);
+    }
+
+    private PostDto mapToDTO(Post post) {
+        PostDto postDto = new PostDto();
+        postDto.setId(post.getId());
+        postDto.setTitle(post.getTitle());
+        postDto.setDescription(post.getDescription());
+        postDto.setContent(post.getContent());
+
+        return postDto;
+    }
+
+    private Post mapToEntity(PostDto postDto){
+        Post post = new Post();
+        post.setTitle(postDto.getTitle());
+        post.setDescription(postDto.getDescription());
+        post.setContent(postDto.getContent());
+
+        return post;
+    }
+}
