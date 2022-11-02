7ec7416074d8e7c80acffd367858ddf20a4ec885
Ke Ma <ke.ma@uconn.edu>
2022-11-01 23:03:10 -0400

    Add files via upload

---
+++ MavenProject/SpringBoot_RedBook/02_post_pageable/src/main/java/com/chuwa/redbook/service/impl/PostServiceImpl.java
@@ -0,0 +1,117 @@
+package com.chuwa.redbook.service.impl;
+
+import com.chuwa.redbook.dao.PostRepository;
+import com.chuwa.redbook.entity.Post;
+import com.chuwa.redbook.exception.ResourceNotFoundException;
+import com.chuwa.redbook.payload.PostDto;
+import com.chuwa.redbook.payload.PostResponse;
+import com.chuwa.redbook.service.PostService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class PostServiceImpl implements PostService {
+    private PostRepository postRepository;
+
+    public PostServiceImpl(PostRepository postRepository) {
+        this.postRepository = postRepository;
+    }
+
+    @Override
+    public PostDto createPost(PostDto postDto) {
+        Post post = mapToEntity(postDto);
+        //post.setTitle(postDto.getTitle());
+        //post.setDescription(postDto.getDescription());
+        //post.setContent(postDto.getContent());
+
+        Post savedPost = postRepository.save(post);
+
+        //postResponse.setId(savedPost.getId());
+        //        //postResponse.setTitle(savedPost.getTitle());
+        //        //postResponse.setDescription((savedPost.getDescription()));
+        //        //postResponse.setContent(savedPost.getContent());
+
+        return mapToDto(savedPost);
+    }
+
+//    @Override
+//    public List<PostDto> getAllPost() {
+//        List<Post> posts = postRepository.findAll();
+//        List<PostDto> postDtos = posts
+//                .stream()
+//                .map(post -> mapToDto(post))
+//                .collect(Collectors.toList());
+//        return postDtos;
+//    }
+
+    @Override
+    public PostDto getPostById(long id) {
+        Post post = postRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Post", "id", id));
+        return mapToDto(post);
+    }
+
+    @Override
+    public PostDto updatePost(PostDto postDto, long id) {
+        Post post = postRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Post", "id", id));
+        post.setTitle(postDto.getTitle());
+        post.setDescription(postDto.getDescription());
+        post.setContent(postDto.getContent());
+
+        Post updatePost = postRepository.save(post);
+        return mapToDto(updatePost);
+    }
+
+    @Override
+    public void deletePostById(long id) {
+        Post post = postRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Post", "id", id));
+        postRepository.delete(post);
+
+    }
+
+    @Override
+    public PostResponse getAllPost(int pageNo, int pageSize, String sortBy, String sortDir) {
+        Sort sort = sortDir.equalsIgnoreCase(Sort.Direction.ASC.name())
+                ? Sort.by(sortBy).ascending()
+                : Sort.by(sortBy).descending();
+        PageRequest pageRequest = PageRequest.of(pageNo, pageSize, sort);
+        Page<Post> pagePosts = postRepository.findAll(pageRequest);
+
+        List<Post> posts = pagePosts.getContent();
+        List<PostDto> postDtos = posts
+                .stream()
+                .map(post -> mapToDto(post))
+                .collect(Collectors.toList());
+
+        PostResponse postResponse = new PostResponse();
+        postResponse.setContent(postDtos);
+        postResponse.setPageNo(pagePosts.getNumber());
+        postResponse.setPageSize(pagePosts.getSize());
+        postResponse.setTotalElements(pagePosts.getTotalElements());
+        postResponse.setLast(pagePosts.isLast());
+        return postResponse;
+    }
+
+    private Post mapToEntity(PostDto postDto) {
+        Post post = new Post();
+        post.setTitle(postDto.getTitle());
+        post.setDescription(postDto.getDescription());
+        post.setContent(postDto.getContent());
+        return post;
+    }
+
+
+    private PostDto mapToDto(Post post) {
+        PostDto postDto = new PostDto();
+        postDto.setId(post.getId());
+        postDto.setTitle(post.getTitle());
+        postDto.setDescription(post.getDescription());
+        postDto.setContent(post.getContent());
+        return postDto;
+    }
+}
+
