4960e2f427760fb5d2bf1502fd173755b46a0fd1
Ke Ma <ke.ma@uconn.edu>
2022-11-03 00:47:46 -0400

    Add files via upload

---
+++ MavenProject/SpringBoot_RedBook/03_comment/src/main/java/com/chuwa/redbook/controller/PostController.java
@@ -0,0 +1,68 @@
+package com.chuwa.redbook.controller;
+
+import com.chuwa.redbook.payload.PostDto;
+import com.chuwa.redbook.payload.PostResponse;
+import com.chuwa.redbook.service.PostService;
+import com.chuwa.redbook.util.AppConstants;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/posts")
+public class PostController {
+
+    @Autowired
+    private PostService postService;
+
+    @PostMapping
+    public ResponseEntity<PostDto> createPost(@RequestBody PostDto postDto){
+        PostDto postResponse = postService.createPost(postDto);
+        return new ResponseEntity<>(postResponse, HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<PostResponse> getAllPost(
+            @RequestParam(value = "pageNo",defaultValue = AppConstants.DEFAULT_PAGE_NUMBER,required = false) int pageNo,
+            @RequestParam(value = "pageSize",defaultValue = AppConstants.DEFAULT_PAGE_SIZE,required = false) int pageSize,
+            @RequestParam(value = "sortBy", defaultValue = AppConstants.DEFAULT_SORT_BY,required = false) String sortBy,
+            @RequestParam(value = "sortDir",defaultValue = AppConstants.DEFAULT_SORT_DIR, required = false) String sortDir
+    ) {
+        PostResponse postResponse = postService.getAllPost(pageNo, pageSize, sortBy, sortDir);
+        return new ResponseEntity<>(postResponse, HttpStatus.OK);
+    }
+
+    @GetMapping("/description")
+    public ResponseEntity<List<PostDto>> findByKeyword(
+            @RequestParam(value = "contains", required = false) String keyword
+    ) {
+        List<PostDto> postDtos = postService.getPostByDescriptionKeyWord(keyword);
+        return new ResponseEntity<>(postDtos, HttpStatus.OK);
+    }
+//    public List<PostDto> getAllPost() {
+//        return postService.getAllPost();
+//    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<PostDto> getPostById(@PathVariable("id") long id) {
+        PostDto postResponse = postService.getPostById(id);
+        return new ResponseEntity<>(postResponse, HttpStatus.OK);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<PostDto> updatePostById(@RequestBody PostDto postDto, @PathVariable(name = "id") long id) {
+        PostDto postResponse = postService.updatePost(postDto, id);
+        return new ResponseEntity<>(postResponse, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deletePost(@PathVariable(name = "id") long id) {
+        postService.deletePostById(id);
+        return new ResponseEntity<>("Post entity deleted successfully.", HttpStatus.OK);
+    }
+
+
+}
