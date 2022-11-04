5b2425d08ffcabb11d17c1fecd2de0a0b104aca8
0000llliii <109409370+0000llliii@users.noreply.github.com>
2022-11-04 10:10:21 -0700

    Add files via upload

---
+++ Coding/Q2/PostJPQLRepositoryImpl.java
@@ -0,0 +1,45 @@
+package com.chuwa.redbook.dao.Impl;
+
+import com.chuwa.redbook.dao.PostJPQLRepository;
+import com.chuwa.redbook.entity.Post;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Repository
+@Transactional
+public class PostJPQLRepositoryImpl implements PostJPQLRepository {
+    @PersistenceContext
+    EntityManager entityManager;
+
+    @Override
+    public List<Post> getAllPostsWithJPQL() {
+        TypedQuery<Post> posts = entityManager.createNamedQuery("Post.getAll", Post.class);
+        return posts.getResultList();
+    }
+
+    public Post insertPost(Post post) {
+        return entityManager.merge(post);
+    }
+
+    public Post updatePost(Post post) {
+        return entityManager.merge(post);
+    }
+
+    public Post getPostById(Long id) {
+        return entityManager.find(Post.class, id);
+    }
+
+    public void delete(Long id) {
+        Post post = entityManager.find(Post.class, id);
+        entityManager.remove(post);
+    }
+
+    public<T> T insertData(T t) {
+        return entityManager.merge(t);
+    }
+}
