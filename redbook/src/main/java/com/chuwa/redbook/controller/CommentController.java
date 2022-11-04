8bcf5cc89b29ab064728ba137d84219d6dad0eaf
Lin27604 <dlin@student.fitchburgstate.edu>
2022-11-04 17:52:43 -0500

    hw10

--- redbook/src/main/java/com/chuwa/redbook/controller/CommentController.java
+++ redbook/src/main/java/com/chuwa/redbook/controller/CommentController.java
@@ -23,16 +23,16 @@ public class CommentController {
         return new ResponseEntity<>(commentService.createComment(id, commentDto), HttpStatus.CREATED);
     }
 
-    @GetMapping("/posts/{podtId}/comments")
-    public List<CommentDto> getCommentsByPostsId(@PathVariable(value = "postId") long podtId) {
-        return commentService.getCommentsByPostId(podtId);
+    @GetMapping("/posts/{postId}/comments")
+    public List<CommentDto> getCommentsByPostsId(@PathVariable(value = "postId") long postId) {
+        return commentService.getCommentsByPostId(postId);
     }
 
-    @GetMapping("/posts/{podtId}/comments/{id}")
-    public ResponseEntity<CommentDto> getCommentsById(@PathVariable(value = "postId") long podtId,
+    @GetMapping("/posts/{postId}/comments/{id}")
+    public ResponseEntity<CommentDto> getCommentsById(@PathVariable(value = "postId") long postId,
                                                       @PathVariable(value = "id") long commentId
                                                       ) {
-        CommentDto commentDto = commentService.getCommentById(podtId, commentId);
+        CommentDto commentDto = commentService.getCommentById(postId, commentId);
         return new ResponseEntity<>(commentDto, HttpStatus.OK);
     }
 
