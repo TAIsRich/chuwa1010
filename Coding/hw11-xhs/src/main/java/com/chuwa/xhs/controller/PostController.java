6a2445ea3825d334580dfa2f0815be30bf58de98
libocui <libo.cui@gmail.com>
2022-11-07 10:49:45 -0800

    hw11 add coding branch 6 and 7

---
+++ Coding/hw11-xhs/src/main/java/com/chuwa/xhs/controller/PostController.java
@@ -0,0 +1,53 @@
+package com.chuwa.xhs.controller;
+
+
+import com.chuwa.xhs.payload.PostDto;
+import com.chuwa.xhs.payload.PostResponse;
+import com.chuwa.xhs.service.PostService;
+import com.chuwa.xhs.util.AppConstants;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v2/xhsposts")
+public class PostController {
+    @Autowired
+    private PostService postService;
+
+    @PostMapping
+    public ResponseEntity<PostDto> createDto(@Valid @RequestBody PostDto postDto){
+        PostDto postResponse =postService.createPost(postDto);
+        return new ResponseEntity<>(postResponse, HttpStatus.CREATED);
+    }
+
+    @GetMapping()
+    public PostResponse getAllPosts(
+            @RequestParam(value = "pageNo", defaultValue = AppConstants.DEFAULT_PAGE_NUMBER, required = false) int pageNo,
+            @RequestParam(value = "pageSize", defaultValue = AppConstants.DEFAULT_PAGE_SIZE, required = false) int pageSize,
+            @RequestParam(value = "sortBy", defaultValue = AppConstants.DEFAULT_SORT_BY, required = false) String sortBy,
+            @RequestParam(value = "sortDir", defaultValue = AppConstants.DEFAULT_SORT_DIR, required = false) String sortDir
+    ) {
+        return postService.getAllPost(pageNo, pageSize, sortBy, sortDir);
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<PostDto> getPostById(@PathVariable(name="id") long id){
+        return ResponseEntity.ok(postService.getPostById(id));
+    }
+    @PutMapping("/{id}")
+    public ResponseEntity<PostDto> updatePostById(@Valid @RequestBody PostDto postDto,@PathVariable(name="id") long id){
+        PostDto postResponse=postService.updatePost(postDto,id);
+        return new ResponseEntity<>(postResponse,HttpStatus.OK);
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deletePost(@PathVariable(name="id") long id){
+        postService.deletePostById(id);
+        return new ResponseEntity<>("Post entity deleted successfully.",HttpStatus.OK);
+    }
+
+
+}
