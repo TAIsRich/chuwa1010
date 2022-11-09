2c6138ca297601793bf61bd90051c019648e93e7
Ke Ma <ke.ma@uconn.edu>
2022-11-09 18:01:30 -0500

    upload files

--- MavenProject/SpringBoot_RedBook/05_validation&actuator/README.md
+++ MavenProject/SpringBoot_RedBook/05_validation&actuator/README.md
@@ -1,1 +1,40 @@
-aaa
+In this branch,
+1. add new dependencies in pom.
+```xml
+<dependency>
+<groupId>org.springframework.boot</groupId>
+<artifactId>spring-boot-starter-validation</artifactId>
+</dependency>
+```
+
+## Validation
+add annotation for fields
+* com.chuwa.redbook.payload.PostDto
+
+* com.chuwa.redbook.payload.CommentDto
+  add @Valid for RequestBody in controller
+
+* com.chuwa.redbook.controller.PostController
+
+* com.chuwa.redbook.controller.CommentController
+
+* com.chuwa.redbook.exception.GlobalExceptionHandler override handleMethodArgumentNotValid method
+
+## Actuator
+
+add dependency
+
+```java
+<dependency>
+<groupId>org.springframework.boot</groupId>
+<artifactId>spring-boot-starteractuator</artifactId>
+</dependency>
+```
+
+add application.properties
+
+```java
+# actuator, JMX
+management.endpoints.web.exposure.include=*
+```
+
---
+++ MavenProject/SpringBoot_RedBook/05_validation&actuator/src/main/java/com/chuwa/redbook/service/impl/PostServiceImpl.java
@@ -0,0 +1,89 @@
+package com.chuwa.redbook.service.impl;
+
+import com.chuwa.redbook.dao.PostRepository;
+import com.chuwa.redbook.entity.Post;
+import com.chuwa.redbook.exception.ResourceNotFoundException;
+import com.chuwa.redbook.payload.PostDto;
+import com.chuwa.redbook.payload.PostResponse;
+import com.chuwa.redbook.service.PostService;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class PostServiceImpl implements PostService {
+
+    @Autowired
+    private PostRepository postRepository;
+
+    //use this modelMapper to replace the mapToDto, mapToEntity methods.
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    @Override
+    public PostDto createPost(PostDto postDto) {
+        Post post = modelMapper.map(postDto, Post.class);
+        Post savedPost = postRepository.save(post);
+        return modelMapper.map(savedPost, PostDto.class);
+    }
+
+    @Override
+    public List<PostDto> getAllPost() {
+        List<Post> posts = postRepository.findAll();
+        List<PostDto> postDtos = posts.stream().map(post -> modelMapper.map(post, PostDto.class)).collect(Collectors.toList());
+        return postDtos;
+    }
+
+    @Override
+    public PostDto getPostById(long id) {
+        Post post = postRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Post", "id", id));
+
+        return modelMapper.map(post, PostDto.class);
+    }
+
+    @Override
+    public PostDto updatePost(PostDto postDto, long id) {
+        Post post = postRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Post", "id", id));
+        post.setTitle(postDto.getTitle());
+        post.setDescription(postDto.getDescription());
+        post.setContent(postDto.getContent());
+
+        Post updatePost = postRepository.save(post);
+        return modelMapper.map(updatePost, PostDto.class);
+    }
+
+    @Override
+    public void deletePostById(long id) {
+        Post post = postRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Post", "id", id));
+        postRepository.delete(post);
+    }
+
+    @Override
+    public PostResponse getAllPost(int pageNo, int pageSize, String sortBy, String sortDir) {
+
+        Sort sort = sortDir.equalsIgnoreCase(Sort.Direction.ASC.name()) ? Sort.by(sortBy).ascending()
+                : Sort.by(sortBy).descending();
+
+        PageRequest pageRequest = PageRequest.of(pageNo, pageSize, sort);
+        Page<Post> pagePosts = postRepository.findAll(pageRequest);
+
+        List<Post> posts = pagePosts.getContent();
+        List<PostDto> postDtos = posts.stream().map(post -> modelMapper.map(post, PostDto.class)).collect(Collectors.toList());
+
+        PostResponse postResponse = new PostResponse();
+        postResponse.setContent(postDtos);
+        postResponse.setPageNo(pagePosts.getNumber());
+        postResponse.setPageSize(pagePosts.getSize());
+        postResponse.setTotalElements(pagePosts.getTotalElements());
+        postResponse.setTotalPages(pagePosts.getTotalPages());
+        postResponse.setLast(pagePosts.isLast());
+        return postResponse;
+    }
+}
