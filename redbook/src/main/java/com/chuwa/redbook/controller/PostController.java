92717a59441e4e2722aa011758e7fa2f261eba10
Rich Peng <rp243@duke.edu>
2022-11-15 18:55:36 -0500

    finish redbook 02 CRUD post

--- redbook/src/main/java/com/chuwa/redbook/controller/PostController.java
+++ redbook/src/main/java/com/chuwa/redbook/controller/PostController.java
@@ -7,18 +7,41 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 
 @RestController
-@RequestMapping("/api/v1")
+@RequestMapping("/api/v1/posts")
 public class PostController {
 
     @Autowired
     private PostService postService;
 
-    @PostMapping("/posts")
+    @PostMapping
     public ResponseEntity<PostDto> createPost(@RequestBody PostDto postDto) {
         PostDto postResponse = postService.createPost(postDto);
         return new ResponseEntity<>(postResponse, HttpStatus.CREATED);
     }
 
+    @GetMapping
+    public List<PostDto> getAllPosts(){
+        return postService.getAllPost();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<PostDto> getPostByID(@PathVariable(name = "id") long id){
+        return ResponseEntity.ok(postService.getPostById(id));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<PostDto> updatePostById(@RequestBody PostDto postDto, @PathVariable(name = "id") long id){
+        PostDto postResponse = postService.updatePost(postDto,id);
+        return new ResponseEntity<>(postResponse, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deletePost(@PathVariable(name = "id") long id){
+        postService.deletePostById(id);
+        return new ResponseEntity<>("Post entity delete successfully", HttpStatus.OK);
+    }
 }
