59defa5931f512c1216fcb0fbe8f1fcc8efe99f6
Rich Peng <rp243@duke.edu>
2022-12-16 19:17:35 -0500

    finished JPQL(session) Feature

--- redbook/src/main/java/com/chuwa/redbook/entity/Post.java
+++ redbook/src/main/java/com/chuwa/redbook/entity/Post.java
@@ -12,6 +12,7 @@ import java.time.LocalDateTime;
         uniqueConstraints = {@UniqueConstraint(columnNames = {"title"})}
 )
 
+@NamedQuery(name="Post.getAll", query="select p from Post p")
 public class Post {
     @Id
     @GeneratedValue(
